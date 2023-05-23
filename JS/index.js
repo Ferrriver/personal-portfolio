17aa377de9e436b0dffbc2b4da4b1d3c08848b20
Fernanda <river.fer.ric@gmail.com>
2023-05-23 16:30:08 -0600

    Styles and functions updated

--- JS/index.js
+++ JS/index.js
@@ -3,10 +3,12 @@ const menu = document.querySelector("#menu");
 const toggleOpen = document.querySelector("#toggle-open");
 const toggleClose = document.querySelector("#toggle-close");
 const $form = document.querySelector("#form");
+const anchor = document.querySelectorAll("#anchor");
 
 toggleOpen.addEventListener('click', toggleMenu);
 toggleClose.addEventListener('click', toggleMenu);
 $form.addEventListener('submit', handleSubmit);
+anchor.forEach(anchor => anchor.addEventListener('click', removeMenu));
 
 function toggleMenu () {
     menu.classList.toggle('show-menu');
@@ -18,6 +20,12 @@ function toggleMenu () {
         toggleOpen.style.display = 'flex';
         toggleClose.style.display = 'none';
     }
+}
+
+function removeMenu () {
+    menu.classList.remove('show-menu');
+    toggleOpen.style.display = 'flex';
+    toggleClose.style.display = 'none';
 }
 
 async function handleSubmit(event) {
